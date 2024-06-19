2c2bbb7e79c14f16f4251516b480d42f5eaec6b5
Ameya-P <ameyaspatkar@gmail.com>
2024-06-18 20:27:49 -0700

    test

--- pages/2_PROJECTS.py
+++ pages/2_PROJECTS.py
@@ -151,12 +151,12 @@ for i in range(2,13):
 data.append("Totals for visible projects:")
 
 for i in range(14,20):
-    data.append(df[i].sum())
+    data.append(df.iloc[:, i].sum())
 
 data.append("")
 
 for i in range(21,109):
-    data.append(df[i].sum())
+    data.append(df.iloc[:, i].sum())
 
 for i in range(109,114):
     data.append("")
@@ -167,7 +167,7 @@ for i in range(115,126):
     data.append("")
 
 for i in range(126,155):
-    data.append(df[i].sum())
+    data.append(df.iloc[:, i].sum())
 
 for i in range(155,158):
     data.append("")
